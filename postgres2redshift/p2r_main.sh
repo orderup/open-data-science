1997fa2a4665d8735260a6dc010618fc8cab65f8
Alex Sleighter <alex@orderup.com>
2015-06-02 14:51:46 -0400

    Use full path to settings.

--- postgres2redshift/p2r_main.sh
+++ postgres2redshift/p2r_main.sh
@@ -6,7 +6,7 @@
   # License: Copyright (c) This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
 
 # Load settings, tables file
-source p2r_settings.sh
+source /app/postgres2redshift/p2r_settings.sh
 
 
 ########################################
