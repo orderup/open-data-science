8f3d7d9f8f7fd85ba8ea53a51e4c65a749868886
Alex Sleighter <alex@orderup.com>
2015-06-02 14:35:17 -0400

    Run clock in profile and import os in migrate

--- migrate.py
+++ migrate.py
@@ -1,2 +1,4 @@
+import os
+
 from subprocess import call
 call(["postgres2redshift/p2r_main.sh", "-h", os.environ['PGHOST'], "-p", os.environ['PGPORT'], "-d", os.environ['PGDB'], "-U", os.environ['PGUSER']])
