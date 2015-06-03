6d35dad7c3e32861d9d20c35356b4b20fe863777
Ubuntu <ubuntu@ip-10-203-192-102.ec2.internal>
2015-06-03 19:39:38 +0000

    dont use access_key at psql command

--- postgres2redshift/p2r_main.sh
+++ postgres2redshift/p2r_main.sh
@@ -130,13 +130,13 @@ date
 # ship original tables
 for table in $TABLES
 do
-  s3cmd put $DATADIR/${table}.txt.gz s3://$S3BUCKET/ --force --access_key $RSKEY --secret_key $RSSECRET --ssl 1>>$STDOUT 2>>$STDERR
+  s3cmd put $DATADIR/${table}.txt.gz s3://$S3BUCKET/ --force 1>>$STDOUT 2>>$STDERR
 done
 
 # ship custom tables
 for table in ${CTNAMES[@]}
 do
-  s3cmd put $DATADIR/${table}.txt.gz s3://$S3BUCKET/ --force --access_key $RSKEY --secret_key $RSSECRET --ssl 1>>$STDOUT 2>>$STDERR
+  s3cmd put $DATADIR/${table}.txt.gz s3://$S3BUCKET/ --force 1>>$STDOUT 2>>$STDERR
 done
 
 echo SHIP TO S3 COMPLETE
